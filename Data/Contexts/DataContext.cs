d79e2850e8e2f0d45915b888c2e9702cd469003c
Parinaz <parinazhss87@gmail.com>
2024-05-27 18:36:06 +0200

    Add project files.

---
+++ Data/Contexts/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace Data.Contexts;
+
+public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<AddressEntity> Addresses { get; set; }
+}
---
+++ Data/Entities/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+namespace Data.Entities;
+
+public class ApplicationUser : IdentityUser
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    public string? Biography { get; set; }
+    public string? ProfileImage { get; set; } = "avatar.jpg";
+
+
+
+    public int? AddressId { get; set; }
+    public AddressEntity? Address { get; set; }
+}
+
+public class AddressEntity
+{
+    [Key]
+    public int Id { get; set; }
+    public string AddressLine_1 { get; set; } = null!;
+    public string? AddressLine_2 { get; set; }
+
+    public string PostalCode { get; set; } = null!;
+
+    public string City { get; set; } = null!;
+}
